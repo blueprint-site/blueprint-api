a3785233dae12f7060d8f5ba33a71be85abb38a1
Timiliris_420 <timothydetournay@gmail.com>
2025-03-11 22:12:11 +0100

    Handle missing environment variables gracefully
    Wrap the main function in a try-catch block to catch errors when required environment variables are missing. Return a 500 status code with the error message in the response.

--- appwrite/functions/get-env/src/main.js
+++ appwrite/functions/get-env/src/main.js
@@ -1,12 +1,18 @@
 const { throwIfMissing } = require('./utils.js');
 
 module.exports = async ({ req, res, log }) => {
-    throwIfMissing(process.env, [
-        'APPWRITE_DATABASE_ID',
-        'APPWRITE_URL',
-        'MEILISEARCH_URL',
-        'MEILISEARCH_SEARCH_API_KEY',
-    ]);
+    try {
+        throwIfMissing(process.env, [
+            'APPWRITE_DATABASE_ID',
+            'APPWRITE_URL',
+            'MEILISEARCH_URL',
+            'MEILISEARCH_SEARCH_API_KEY',
+        ]);
 
-    res.json(process.env);
+        res.json(process.env);
+        return;
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    }
 };
