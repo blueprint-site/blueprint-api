acc897a6e178d97b59610d03e4979bcd7bc77fa9
Timiliris_420 <timothydetournay@gmail.com>
2025-03-11 22:38:06 +0100

    Remove CORS handling
    This commit removes the CORS handling logic from the function. CORS should be handled at the Appwrite level.

--- appwrite/functions/get-env/src/main.js
+++ appwrite/functions/get-env/src/main.js
@@ -1,22 +1,7 @@
 const { throwIfMissing } = require('./utils.js');
 
-const ALLOWED_ORIGINS_ENV_VAR = process.env.ALLOWED_ORIGINS; // Get the env var
-
-const ALLOWED_ORIGINS = ALLOWED_ORIGINS_ENV_VAR ? ALLOWED_ORIGINS_ENV_VAR.split(',') : []; // Split the string into an array
-
 module.exports = async ({ req, res, log }) => {
     try {
-        const origin = req.headers['origin'];
-        let corsMessage = null;
-
-        if (ALLOWED_ORIGINS.includes(origin)) {
-            res.setHeader('Access-Control-Allow-Origin', origin);
-        } else {
-            corsMessage = `CORS violation: Origin ${origin} is not allowed.`;
-            log(corsMessage); // Added log
-            return res.json({ error: 'CORS policy violation', log: corsMessage }); // Changed res.status(403).json to res.json and added log
-        }
-
         const requiredKeys = [
             'APPWRITE_DATABASE_ID',
             'APPWRITE_URL',
@@ -31,15 +16,10 @@ module.exports = async ({ req, res, log }) => {
             return obj;
         }, {});
 
-        const response = { ...filteredEnv };
-        if (corsMessage) {
-            response.log = corsMessage;
-        }
-
-        return res.json(response);
+        return res.json(filteredEnv);
 
     } catch (error) {
         console.error(error);
-        return res.json({ error: error.message }); // Changed res.status(500).json to res.json
+        return res.status(500).json({ error: error.message });
     }
 };
