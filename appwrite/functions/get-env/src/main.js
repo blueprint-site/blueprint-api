6875857d9287a3384a5708b255fe964ad6b2ee22
Timiliris_420 <timothydetournay@gmail.com>
2025-03-11 22:22:35 +0100

    Enable CORS with allowed origins from environment variable

--- appwrite/functions/get-env/src/main.js
+++ appwrite/functions/get-env/src/main.js
@@ -1,7 +1,19 @@
 const { throwIfMissing } = require('./utils.js');
 
+const ALLOWED_ORIGINS_ENV_VAR = process.env.ALLOWED_ORIGINS; // Get the env var
+
+const ALLOWED_ORIGINS = ALLOWED_ORIGINS_ENV_VAR ? ALLOWED_ORIGINS_ENV_VAR.split(',') : []; // Split the string into an array
+
 module.exports = async ({ req, res, log }) => {
     try {
+        const origin = req.headers['origin'];
+
+        if (ALLOWED_ORIGINS.includes(origin)) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        } else {
+            return res.status(403).json({ error: 'CORS policy violation' });
+        }
+
         const requiredKeys = [
             'APPWRITE_DATABASE_ID',
             'APPWRITE_URL',
